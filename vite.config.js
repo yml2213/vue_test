63fca9c4799f5ed472f3d3aa83c2c45bc357f111
cym <cymznz@qq.com>
2024-04-22 22:19:34 +0800

    设置反代

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    https: false,
+    proxy: {
+      '/api': {
+        target: 'https://localhost:3000', // 接口的域名
+        secure: false, // 如果是https接口，需要配置这个参数
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
